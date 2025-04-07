901bdd898f8308990d03cd6d351305897093400b
Weii Wang <weii.wang@canonical.com>
2025-04-07 13:16:23 +0800

    FIX

--- http-proxy-policy-operator/tests/integration/test_charm.py
+++ http-proxy-policy-operator/tests/integration/test_charm.py
@@ -57,5 +57,8 @@ async def test_proxy_requests(
         "http": "http://test:test@proxy.test",
         "https": "https://test:test@proxy.test",
     }
+    assert await requirer_charm_a.get_proxy_status() == http_proxy.PROXY_STATUS_READY
     assert await requirer_charm_b.get_proxies() is None
+    assert await requirer_charm_b.get_proxy_status() == http_proxy.PROXY_STATUS_REJECTED
     assert await requirer_charm_c.get_proxies() is None
+    assert await requirer_charm_c.get_proxy_status() == http_proxy.PROXY_STATUS_PENDING
